c0dd803149bfed1e5b702dd30358839c1bfc01c3
Charles <hallard04@free.fr>
2018-03-21 19:23:09 +0100

    Default enable gwtraf

--- set_config.py
+++ set_config.py
@@ -278,7 +278,7 @@ server['serv_type'] = "gwtraf"
 server['server_address'] = "127.0.0.1"
 server['serv_port_up'] = 1688
 server['serv_port_down'] = 1689
-server['serv_enabled'] = False
+server['serv_enabled'] = True
 gateway_conf['servers'].append(server)
 
 # Add Local LoRaWAN server
