cbf8068d4c9dcabc18d9e22a6b5a862f45447ae2
Charles <hallard04@free.fr>
2018-08-24 14:57:48 +0200

    Removed reset pin as input after reset

--- start.sh
+++ start.sh
@@ -16,15 +16,21 @@ WAIT_GPIO() {
 # cleanup GPIO
 if [ -d /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN ]
 then
-  echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+  echo "GPIO$SX1301_RESET_BCM_PIN Already available"
+else
+  echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/export; WAIT_GPIO
 fi
 
 # setup GPIO
-echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/export; WAIT_GPIO
-echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction; WAIT_GPIO
-echo "1" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value; WAIT_GPIO
-echo "0" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value; WAIT_GPIO
-echo "in" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction; WAIT_GPIO
+echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
+WAIT_GPIO
+echo "1" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+WAIT_GPIO
+echo "0" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+WAIT_GPIO
+
+# removed, prevent start on IMST Gateway Lite
+#echo "in" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
 
 # Fire up the forwarder.
 ./mp_pkt_fwd
